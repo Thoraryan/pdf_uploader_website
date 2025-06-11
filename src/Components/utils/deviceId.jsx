15cd4e2d4e79a8f24d31500714d1c6f5f0a7b9d0
Aryan Saini <aryansainidevloper@gmail.com>
2025-06-11 23:52:27 +0530

    change

--- src/Components/utils/deviceId.jsx
+++ src/Components/utils/deviceId.jsx
@@ -1,21 +1,18 @@
 export const getDeviceId = () => {
   try {
     let deviceId = localStorage.getItem("deviceId");
-    if (!deviceId) {
-      const generateUUID = () => {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-          const r = Math.random() * 16 | 0;
-          const v = c === 'x' ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
-        });
-      };
 
-      deviceId = typeof crypto.randomUUID === 'function'
-        ? crypto.randomUUID()
-        : generateUUID();
+    if (!deviceId) {
+      // Always generate manually (no crypto dependency)
+      deviceId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
 
       localStorage.setItem("deviceId", deviceId);
     }
+
     return deviceId;
   } catch (error) {
     console.error("Failed to generate deviceId:", error);
